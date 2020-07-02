ecdfda5ec53dd3a73b05c58b840164469c0f80a4
oscar portillo <oscar9414ps@gmail.com>
2020-07-02 17:33:43 +0200

    9

--- PHP Developer/Php Básico online/Formularios/index.php
+++ PHP Developer/Php Básico online/Formularios/index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Formulario ejemplo</title>
-</head>
-<body>
-  <br>
-  <h3>Ejemplo de formulario</h3>
-  <form action="form.php" method="post">
-    
-    Nombre:
-    <input type="text" id="nombre" name="nombre"><br>
-    Email
-    <input type="text" id="email" name="email">
-    <br><br>
-    <input type="Submit" id="btn_enviar" name="btn_enviar" value=" Enviar ">
+  <head>
+    <title>Formulario ejemplo</title>
+  </head>
+  <body>
+    <br>
+    <h3>Ejemplo de formulario</h3>
+    <form action="form.php" method="post">
 
-  </form>
+      Nombre:
+      <input type="text" id="nombre" name="nombre"><br>
+      Email
+      <input type="text" id="email" name="email">
+      <br><br>
+      <input type="Submit" id="btn_enviar" name="btn_enviar" value=" Enviar ">
 
-</body>
+    </form>
+
+  </body>
 </html>
